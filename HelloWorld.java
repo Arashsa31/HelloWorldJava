9f4a6ad065fe315ca126b6003ea0c4f9c7631407
Arash Sa <101516570+Arashsa31@users.noreply.github.com>
2022-03-26 12:00:43 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,13 +1,11 @@
 /**
  * @author Arash
- *
+ * This program prints the famous Hello World! text.
  */
-public class HelloWorld {
-
-	public static void main(String[] args) {
-
+public class HelloWorld 
+{
+	public static void main(String[] args) 
+	{
 		System.out.println("Hello World!");
-
 	}
-
 }
